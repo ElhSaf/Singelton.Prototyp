c7792bd8f771b861eaade47d9c1f3656202b6422
ElhSaf <elham_abdul@hotmail.com>
2022-02-16 13:22:24 +0100

    New interface Ex.

---
+++ ConsoleApp3/Interface/IUser.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singelton.Protoyp.Interface
+{  // denna interface måste skapas mot en klass i det här fallet Klass User.
+   internal interface IUser  //När det börjar med stort I är det alltid interface    //interface klass måste man ändra från klass "interfave"
+    {
+        public void ShowName();     //skapr metoder som sk användas
+        public void PrintInfo(); // samma här
+
+        public int Id { get; set; }
+
+
+
+    }
+}
---
+++ ConsoleApp3/Interface/Interface.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singelton.Protoyp.Interface
+{
+
+
+    class User : IUser
+
+    {
+        private int _id;
+        private int Id
+        {
+            get
+            {
+                return _id;
+            }
+            set
+            {
+                _id = value;
+            }
+        }
+
+        int IUser.Id { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public void ShowName()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PrintInfo()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
